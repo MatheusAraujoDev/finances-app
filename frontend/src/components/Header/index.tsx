62bff5caa5d9259232eab506bfc45cabfbabc02c
Matheus <matheusaraujo.dev@gmail.com>
2023-05-16 14:19:23 -0300

    Refactor: change app name

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -14,7 +14,7 @@ export default function Header() {
     <header className='bg-purple-900 h-28'>
       <div className="flex h-full items-center justify-between p-5">
         <p className="text-white text-xl">{t("walletPage.welcome")} <b className="text-green-400 text-xl">{username}</b></p>
-        <h1 className="flex h-full items-center justify-center text-white text-4xl">DinDin Finances<img src="/wallet.png" height={40} width={40} className="ml-1" /></h1>
+        <h1 className="flex h-full items-center justify-center text-white text-4xl">Smart Finances<img src="/wallet.png" height={40} width={40} className="ml-1" /></h1>
         <LanguageSelect />
       </div>
     </header>
--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <title>DinDin Wallet</title>
+      <title>Smart Finances</title>
       <link rel="icon" href="/wallet.png" />
       <body>
         <Main />
